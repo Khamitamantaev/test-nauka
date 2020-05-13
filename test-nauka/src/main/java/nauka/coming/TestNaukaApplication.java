f5c4a44be0f3b0b9d362b1b6c973eeb197e5401d
Khamit <khamitamantaev@gmail.com>
2020-05-13 20:18:01 +0500

    project has been created!

---
+++ test-nauka/src/main/java/nauka/coming/TestNaukaApplication.java
@@ -0,0 +1,13 @@
+package nauka.coming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNaukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNaukaApplication.class, args);
+	}
+
+}
---
+++ test-nauka/src/test/java/nauka/coming/TestNaukaApplicationTests.java
@@ -0,0 +1,13 @@
+package nauka.coming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNaukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
